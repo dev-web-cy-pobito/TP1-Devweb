0aa2f781e76963c0c2d703609426e7dbb58e2acd
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-01-26 09:40:11 +0100

    js

---
+++ html/assets/js/populate.js
@@ -0,0 +1,33 @@
+const puffles = [
+    {
+        "name": "Red Puffle",
+        "colors": "red",
+        "pic_url": "assets/images/logo.png",
+        "description": "lorem ipsum in dolor",
+        "price": 100,
+        "categories": [
+            "baisc"
+        ]
+    },
+    {
+        "name": "Red Puffle",
+        "colors": "red",
+        "pic_url": "assets/images/logo.png",
+        "description": "lorem ipsum in dolor",
+        "price": 100,
+        "categories": [
+            "baisc"
+        ]
+    },
+    {
+        "name": "Red Puffle",
+        "colors": "red",
+        "pic_url": "assets/images/logo.png",
+        "description": "lorem ipsum in dolor",
+        "price": 100,
+        "categories": [
+            "baisc"
+        ]
+    },
+
+]
