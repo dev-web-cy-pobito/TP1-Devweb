1900247ff76ab4ed73aacce976599f4ba226dc1b
Samuel <sam200133@gmail.com>
2022-04-07 12:07:19 +0200

    puffles

--- php/fill_db.php
+++ php/fill_db.php
@@ -1,15 +1,13 @@
 <?php
 
+include_once "../config.php";
 include_once 'db.php';
 
-$servername = "localhost";
-$username = "abc";
-$password = "freforlife";
 $json_jobs_path = "../data/activity.json";
+$json_jobs_path = "../data/puffles.json";
 
 // Create connection
-$db = new DB($servername,'Puffles', $username,$password);
-
+$db = new DB($_CONFIG['db_host'],'Puffles', $_CONFIG['db_username'],$_CONFIG['db_password']);
 
 // Add jobs to the DB
 $json_jobs = file_get_contents($json_jobs_path);
@@ -24,5 +22,20 @@ foreach ($jobs_full as $cat) {
       $db->setSimpleValues('job_to_cat', ['job_id'=> $db->getLastInsertId(), 'category_id'=> $cat_id], $id=false, $ignore=true);
   }
 }
+
+// Add puffles to DB
+$json_puffles = file_get_contents($json_puffles_path);
+$puffles_full = json_decode($json_puffles);
+
+foreach ($puffles as $puffle) {
+  $db->setSimpleValues('puffles',[
+    'name' => $puffle->name,
+    'pic_url' => $puffle->pic_url,
+    'description' => $puffle->description,
+    'stock' => $puffle->stock,
+    'price' => ($puffle->price != null) ?  $puffle->price : 0,
+]);
+}
+
 
 ?>
